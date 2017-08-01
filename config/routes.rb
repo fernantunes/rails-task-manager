8e87bdbedfd107065032f412abba040dd92265aa
Fernando Antunes <fernandoantunes@gmail.com>
2017-08-01 15:06:28 -0300

    Basic App Ready and DueDate added to Tasks

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    get 'tasks/index'
+    get 'tasks/show'
+    get 'tasks/new'
+    get 'tasks/edit'
+    resources :tasks
+
+=begin
+  get '/tasks', to: "tasks#index"
+  get '/tasks/new', to: "tasks#new", as: 'new_task'
+  get '/tasks/:id', to: "tasks#show"
+  post '/tasks', to: "tasks#create"
+  get '/tasks/:id/edit', to: "tasks#edit", as: 'edit_task'
+  patch '/tasks/:id', to: "tasks#update"
+  delete '/tasks/:id', to: "tasks#destroy", as: 'task'
+=end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170801142813) do
+ActiveRecord::Schema.define(version: 20170801180407) do
 
   create_table "tasks", force: :cascade do |t|
     t.string   "name"
